421c209194e94a90645812fdaae351089293f9f4
wawanpi <muhaguskurniawan@gmail.com>
2023-04-07 09:50:04 +0700

    Add project files.

---
+++ easd_1_202_D/easd_1_202_D.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+// 1.Suatu algoritma dibutuhkan untuk menyelesaikan suatu masalah karena algoritma 
+// merupakan langkah-langkah atau prosedur terstruktur yang dapat diikuti untuk
+// menyelesaikan masalah tertentu secara efektif dan efisien. Dengan menggunakan 
+// algoritma, kita dapat memecahkan masalah dalam waktu yang lebih singkat dan dengan
+// cara yang lebih terorganisir dan sistematis.
+int main()
+{
+    std::cout << "Hello World!\n";
+}
