c2ff248d747138141b0fbed80fe4b5230a6cbab3
wawanpi <muhaguskurniawan@gmail.com>
2023-04-07 10:15:52 +0700

    no3

--- easd_1_202_D/easd_1_202_D.cpp
+++ easd_1_202_D/easd_1_202_D.cpp
@@ -8,7 +8,25 @@ using namespace std;
 // cara yang lebih terorganisir dan sistematis.
 
 // 2.Dalam algoritma, terdapat dua jenis data struktur yaitu
-//  A.Linear data structure: data disimpen secara berurutan dan dapat di akses secara langsung melalu indeks.
+//		A.Linear data structure: data disimpen secara berurutan dan dapat di akses secara langsung melalu indeks.
 //  contoh : array, link list
-//  B. Non-linear data structure: data disimpan tidak secara berurutan dan tidak dapat diakses secara langsung melalui 
+//		 B. Non-linear data structure: data disimpan tidak secara berurutan dan tidak dapat diakses secara langsung melalui 
 //  melalui indeks. Contoh: tree, graph.
+
+// 3. Waktu eksekusi: Waktu yang dibutuhkan oleh program untuk menyelesaikan tugasnya sangat mempengaruhi efisiensi.
+//	  Semakin cepat program dapat menyelesaikan tugasnya, semakin efisien program tersebut.
+
+//	  Jenis algoritma: Jenis algoritma yang digunakan dalam program dapat mempengaruhi efisiensi program.
+//	  Beberapa algoritma lebih efisien daripada yang lain dalam menyelesaikan tugas yang sama.
+
+//	  Penggunaan struktur data yang tepat: Struktur data yang tepat dapat meningkatkan efisiensi program dengan meminimalkan waktu dan memori
+//	  yang digunakan dalam program.
+
+//	  Ukuran input: Semakin besar ukuran input, semakin sulit untuk membuat program yang efisien. Oleh karena itu, program yang efisien harus mampu
+//	  menangani input yang besar dengan cepat dan tanpa memakan terlalu banyak memori.
+
+//	  Penggunaan teknik pemrograman yang efisien: Teknik pemrograman seperti rekursi, iterasi, atau metode perhitungan numerik yang tepat dapat 
+//	  efisiensi program.
+
+
+
